826018a34fa847b2b0938f30b12db639ffddd79f
João Arthur <jao.arthurag@gmail.com>
2023-08-08 16:08:34 -0300

    build: naming server added

--- config-server/src/main/java/com/compass/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/compass/configserver/ConfigServerApplication.java
@@ -3,8 +3,9 @@ package com.compass.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableConfigServer
+@EnableEurekaServer
 @SpringBootApplication
 public class ConfigServerApplication {
 
--- messaging/src/main/java/com/compass/messaging/MessagingApplication.java
+++ messaging/src/main/java/com/compass/messaging/MessagingApplication.java
@@ -9,5 +9,4 @@ public class MessagingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MessagingApplication.class, args);
 	}
-
 }
